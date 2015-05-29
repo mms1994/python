0768a160aa81517bc78c761ed069f4d7f2bb8650
Michał Spirała <michal.spirala@gmail.com>
2015-05-29 14:10:11 +0200

    up

--- 10.py
+++ 10.py
@@ -3,12 +3,15 @@ f = open("scr.txt", "r")
 a=0
 b=0
 slownik={}
-for line in f:
+line=f.readline()
+while(line!=""):
+    line=line.replace("\n", "")
     temp=line.split(' ')
     if str(temp[0])=='set':
         slownik[temp[1]]=temp[2]
-    print(a)
-print(b)
+    elif str(temp[0])=='add':
+        slownik[temp[1]]=slownik[temp[1]]+slownik[temp[2]]
+    line=f.readline()
 print(slownik)
 #2
 
