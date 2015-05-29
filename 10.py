de9365cef4901b39e48b5fcc7914a20baddcefc5
Michał Spirała <michal.spirala@gmail.com>
2015-05-29 13:55:59 +0200

    10

--- 10.py
+++ 10.py
@@ -1,5 +1,15 @@
 #1
-
+f = open("scr.txt", "r")
+a=0
+b=0
+slownik={}
+for line in f:
+    temp=line.split(' ')
+    if str(temp[0])=='set':
+        slownik[temp[1]]=temp[2]
+    print(a)
+print(b)
+print(slownik)
 #2
 
 #3
