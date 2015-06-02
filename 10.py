c8da29dfc0144ea635265baa86170b187277cf4a
mms1994 <michal.spirala@hotmail.com>
2015-06-02 20:02:54 +0200

    mnożenie

--- 10.py
+++ 10.py
@@ -4,15 +4,23 @@ a=0
 b=0
 slownik={}
 line=f.readline()
-while(line!=""):
+while(line!=EOFError):
     line=line.replace("\n", "")
     temp=line.split(' ')
     if str(temp[0])=='set':
         slownik[temp[1]]=temp[2]
     elif str(temp[0])=='add':
         slownik[temp[1]]=int(slownik[temp[1]])+int(slownik[temp[2]])
+    elif str(temp[0])=='mul':
+        slownik[temp[1]]=int(slownik[temp[1]])*int(slownik[temp[2]])
+    elif str(temp[0])=='print':
+        print(slownik[temp[1]])
+    elif str(temp[0])=='noop':
+        continue
+    elif str(temp[0])=='read':
+        #wczytywanie
+        continue
     line=f.readline()
-print(slownik)
 #2
 
 #3
