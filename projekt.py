4ea15b3b9678ee25eea48fe822404781ef332f6a
mms1994 <michal.spirala@hotmail.com>
2015-06-12 00:24:13 +0200

    projekt

--- projekt.py
+++ projekt.py
@@ -1,3 +1,4 @@
+import math
 from numpy import zeros, diag, diagflat, dot
 
 class SLESolver:
@@ -10,6 +11,15 @@ class jacob(SLESolver):
     A = []
     b = []
     line = 0
+    x2 = []
+    koniec='end'
+    EST = []
+    def norma_MAX(self, vec):
+        MAX = math.fabs(vec[0])
+        for i in range(1, int(self.line)):
+            if math.fabs(vec[i]) > MAX:
+                MAX = math.fabs(vec[i])
+        return MAX
     def solveGenerator (self , inputFile , N , tol ):
         f = open(inputFile, "r")
         self.line=f.readline()
@@ -27,23 +37,36 @@ class jacob(SLESolver):
             next=next.replace("\n", "")
             self.b[i]=next
         self.x = zeros(int(self.line))
+        self.x2 = zeros(int(self.line))
+        self.EST = zeros(int(self.line))
         D = diag(self.A)
         R = self.A - diagflat(D)
         for i in range(N):
             self.x = (self.b - dot(R,self.x)) / D
-            yield self.x
+            for z in range(int(self.line)):
+                self.EST[z]= math.fabs(self.x[z]-self.x2[z])
+            max=self.norma_MAX(self.EST)
+            if(max < tol):
+                yield self.koniec
+            else:
+                self.x2 = self.x
+                yield self.x
         f.close
     def solve (self , inputFile , outputFile , intermediateResultsFile , N , tol ):
         gen = self.solveGenerator(inputFile, N, tol)
         f3 = open(intermediateResultsFile,  'w')
         for i in range(N):
             ret=next(gen)
-            f3.write(str(i))
-            f3.write(' ')
-            for j in range(len(ret)):
-                f3.write(str(ret[j]))
+            if(ret=='end'):
+                ret=ret2
+            else:
+                ret2=ret
+                f3.write(str(i))
                 f3.write(' ')
-            f3.write('\n')
+                for j in range(len(ret)):
+                    f3.write(str(ret[j]))
+                    f3.write(' ')
+                f3.write('\n')
         f2 = open(outputFile, 'w')
         f2.write(str(len(ret)))
         f2.write('\n')
