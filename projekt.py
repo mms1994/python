5cc30f307aaeeb432c98a7451a4fb856dad92f4e
Michał Spirała <michal.spirala@gmail.com>
2015-06-12 13:02:05 +0200

    11

--- projekt.py
+++ projekt.py
@@ -2,10 +2,13 @@ import math
 from numpy import zeros, diag, diagflat, dot
 
 class SLESolver:
-    def solveGenerator (self , inputFile , N, tol ):
+    def solveGenerator(self, inputFile, N, tol):
         pass
-    def solve (self , inputFile , outputFile , intermediateResultsFile , N, tol ):
+
+    def solve (self, inputFile, outputFile, intermediateResultsFile, N, tol):
         pass
+
+
 class jacob(SLESolver):
     x = []
     A = []
@@ -20,22 +23,22 @@ class jacob(SLESolver):
             if math.fabs(vec[i]) > MAX:
                 MAX = math.fabs(vec[i])
         return MAX
-    def solveGenerator (self , inputFile , N , tol ):
+    def solveGenerator(self, inputFile, N, tol):
         f = open(inputFile, "r")
-        self.line=f.readline()
-        size=(int(self.line), int(self.line))
+        self.line = f.readline()
+        size = (int(self.line), int(self.line))
         self.A = zeros(size)
         for i in range(int(self.line)):
-            next=f.readline()
-            next=next.replace("\n", "")
-            temp=next.split(' ')
+            next = f.readline()
+            next = next.replace("\n", "")
+            temp = next.split(' ')
             for j in range(int(self.line)):
-                self.A[i][j]=temp[j]
+                self.A[i][j] = temp[j]
         self.b = zeros(int(self.line))
         for i in range(int(self.line)):
-            next=f.readline()
-            next=next.replace("\n", "")
-            self.b[i]=next
+            next = f.readline()
+            next = next.replace("\n", "")
+            self.b[i] = next
         self.x = zeros(int(self.line))
         self.x2 = zeros(int(self.line))
         self.EST = zeros(int(self.line))
@@ -44,20 +47,21 @@ class jacob(SLESolver):
         for i in range(N):
             self.x = (self.b - dot(R,self.x)) / D
             for z in range(int(self.line)):
-                self.EST[z]= math.fabs(self.x[z]-self.x2[z])
-            max=self.norma_MAX(self.EST)
+                self.EST[z] = math.fabs(self.x[z]-self.x2[z])
+            max = self.norma_MAX(self.EST)
             if(max < tol):
                 yield self.koniec
             else:
                 self.x2 = self.x
                 yield self.x
         f.close
-    def solve (self , inputFile , outputFile , intermediateResultsFile , N , tol ):
+
+    def solve (self, inputFile, outputFile, intermediateResultsFile, N, tol):
         gen = self.solveGenerator(inputFile, N, tol)
         f3 = open(intermediateResultsFile,  'w')
         for i in range(N):
             ret=next(gen)
-            if(ret=='end'):
+            if(ret == 'end'):
                 ret=ret2
             else:
                 ret2=ret
@@ -79,7 +83,7 @@ class jacob(SLESolver):
                 print("{0:10.3e}".format(self.A[i][j]) , end=' ')
             print( ' ] ', end=' ')
             print('[ ' + '{0:10.3e}'.format(self.x[i]) + ' ] ', end=' ')
-            if(i==int(self.line)-1):
+            if(i == int(self.line)-1):
                 print(' = ', end=' ')
             else:
                 print('   ', end=' ')
@@ -87,5 +91,6 @@ class jacob(SLESolver):
         f2.close
         f3.close
         return ret
+
 SLS=jacob()
 sol = SLS.solve("input.txt", "output.txt", "inter.txt", 25, 0.001)
