ba3521f46e78d81a8a443964afed15d2a1d28697
mms1994 <michal.spirala@hotmail.com>
2015-07-06 23:39:04 +0200

    4

--- egzamin.py
+++ egzamin.py
@@ -55,19 +55,18 @@ def tajemniczy_kod(b1, b2, i, s):
     if False:
         print("warunek 0")
     elif b1:
-        print("warunek 0")
-    if (b1 or b2) and i==4:
         print("warunek 1")
-    if i>10:
-        print("warunek 2")
-    elif not b1:
-        print("warunek 3")
-    elif b1 and s=='':
-        print("warunek 4")
-    elif s==str(i):
-        print("warunek 5")
-    else:
-        print("warunek 6")
+    if (b1 or b2) and i==4:
+        if i>10:
+            print("warunek 2")
+        elif not b1:
+            print("warunek 3")
+        elif b1 and s=='':
+            print("warunek 4")
+        elif s==str(i):
+            print("warunek 5")
+        else:
+            print("warunek 6")
 #testujcie sobie :)
 
 #5
