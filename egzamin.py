875e19a1c0db3b4a021a26824f9a2e2d886b9d1c
mms1994 <michal.spirala@hotmail.com>
2015-07-06 23:24:28 +0200

    1

--- egzamin.py
+++ egzamin.py
@@ -1,4 +1,18 @@
+#1
+print("zad1")
+print(7/5)
+print(7//5)
+print(7%5)
+print("poza zakresem")
+print("poza zakresem")
+print("nie mozna tak zrobic podmiany")
+print("chuj wie, dużo przepisywania")
+litery=['a', 'b', 'c', 'd']
+litery[1]=321
+print(litery)
+
 #6
+print("zad6")
 def zmien(str):
     str2=""
     for i in range(len(str)):
@@ -15,6 +29,7 @@ stri2=zmien(stri)
 print(stri2)
 
 #8
+print("zad8")
 def dni(miesiac, rok):
     if (miesiac>0 and miesiac<13 and rok>0):
         if(miesiac==1 or miesiac==3 or miesiac==5 or miesiac==7 or miesiac==8 or miesiac==10 or miesiac ==12):
@@ -37,6 +52,7 @@ def dni(miesiac, rok):
 print(dni(2,1900))
 
 #9
+print("zad9")
 class Okrag:
     promien=0
     def __init__(self, pr):
